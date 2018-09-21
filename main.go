12538bb7c2650bb166be17bf542d98fbb4830c13
Shota Yoshihara <yoshihara-s@klab.com>
2018-09-21 16:49:15 +0900

    scrape id from search list

--- main.go
+++ main.go
@@ -3,20 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/PuerkitoBio/goquery"
+	youtube "github.com/kaneta1992/go-youtube-id/src"
 )
 
 func main() {
-	doc, err := goquery.NewDocument("https://www.youtube.com/results?search_query=%E5%8A%A0%E8%97%A4%E7%B4%94%E4%B8%80&sp=EgIQAQ%253D%253D")
-	if err != nil {
-		panic(err)
-	}
-
-	selection := doc.Find("h3 a")
-	cnt := 0
-	selection.Each(func(index int, s *goquery.Selection) {
-		fmt.Println(s.AttrOr("href", "not exists"))
-		cnt++
-	})
-	fmt.Println(cnt)
+	c := youtube.NewClient("加藤純一")
+	fmt.Println(c.GetVideos().First())
+	fmt.Println(c.GetVideos().Last())
 }
---
+++ src/youtube.go
@@ -0,0 +1,34 @@
+package youtube
+
+import (
+	"net/url"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type Client struct {
+	videos Videos
+}
+
+func NewClient(query string) *Client {
+	c := &Client{}
+	encoded := url.QueryEscape(query)
+	doc, err := goquery.NewDocument("https://www.youtube.com/results?search_query=" + encoded + "&sp=EgIQAQ%253D%253D")
+	if err != nil {
+		panic(err)
+	}
+
+	selection := doc.Find("h3 a")
+	selection.Each(func(index int, s *goquery.Selection) {
+		if id, ok := s.Attr("href"); ok {
+			// "/watch?v="を切り取る
+			id = id[9:]
+			c.videos = append(c.videos, NewVideo(id, nil))
+		}
+	})
+	return c
+}
+
+func (c *Client) GetVideos() Videos {
+	return c.videos
+}
